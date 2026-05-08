44f6f3a634c678780c17b52f5ba8649efefd48c9
Mario <jhonmariorestrepo890@gmail.com>
2026-05-08 17:13:15 -0500

    feat(users): agregar serializadores de usuario y esquemas de vistas

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'password', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
