bc617c3d5b6fbd007eadc459518e73e68c56faeb
Mario <jhonmariorestrepo890@gmail.com>
2026-05-14 16:09:27 -0500

    feat(config): registrar la aplicación de proyectos y configurar la localización

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users',
+    'apps.projects',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -110,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
